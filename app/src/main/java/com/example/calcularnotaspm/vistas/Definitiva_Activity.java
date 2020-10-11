7f9f261af0f322bcb24a1c914fd6b2ffd250a71e
ayolethangulo <anayoleth93@gmail.com>
2020-10-11 18:43:17 -0500

    diseño de la definitivas

---
+++ app/src/main/java/com/example/calcularnotaspm/vistas/Definitiva_Activity.java
@@ -0,0 +1,16 @@
+package com.example.calcularnotaspm.vistas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.calcularnotaspm.R;
+
+public class Definitiva_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_definitiva_);
+    }
+}
